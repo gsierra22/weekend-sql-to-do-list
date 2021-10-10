93ef3c81c10854c986827d9b0ae28754582a2938
Giancarlo Sierra <giancarlosierra2011@hotmail.com>
2021-10-10 12:50:26 -0500

    get route

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,30 @@
 $(document).ready(onReady);
 
 function onReady(){
-    console.log('hello!')
+    $('#enterTaskButton').on('click', sendTask)
+}
+
+
+function getTask(){
+    $.ajax({
+        method:'GET',
+        url:'/list'
+    }).then (function(response){
+        let el= $('#outputTasks')
+        el.empty()
+        for(let i=0; i<response.length;i++){
+            let appendTask= `<li>${ response[i].author }: ${ response[i].body_text }</li>`;
+            el.append(appendTask)
+        }
+    }).catch(function(err){
+        console.log(err);
+        alert('There was an issue adding items')
+    })
+}
+function sendTask(){
+    let taskToSend={
+        tasks: $('#taskIn').val(),
+        task_completed:$('#taskCompleteIn').val()
+    }
+    console.log('sending', taskToSend)
 }
